8ab9ddbd70e9cff9b70a1822af20a678ed961f10
Sir-Uus <firdauswho7@gmail.com>
2024-06-04 10:34:43 +0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,387 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dbService = require("./dbService");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Agar bisa akses file statis dari direktori client
+app.use(express.static(path.join(__dirname, "..", "client")));
+
+// Akses Bootstrap
+app.get("/css/bootstrap.css", (req, res) => {
+  res.sendFile(__dirname + "/node_modules/bootstrap/dist/css/bootstrap.css");
+});
+app.get("/js/bootstrap.js", (req, res) => {
+  res.sendFile(__dirname + "/node_modules/bootstrap/dist/js/bootstrap.js");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
+
+app.set("views", path.join(__dirname, "..", "client", "views"));
+app.set("view engine", "ejs");
+
+// PRODUK Table
+app.get("/produk", (req, res) => {
+  const db = dbService.getDbServiceInstance();
+
+  db.getAllProduk()
+    .then((data) => {
+      if (!Array.isArray(data)) {
+        console.log("Error:", data);
+        data = [];
+      }
+      res.render("produk/indexProduk", { myProduk: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("produk/indexProduk", { myProduk: [] });
+    });
+});
+
+app.post("/produk/insert", (req, res) => {
+  const { namaproduk, stock, hargasatuan } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.insertNewProduk(namaproduk, stock, hargasatuan)
+    .then((insertedData) => {
+      if (insertedData) {
+        res.redirect("/produk");
+      } else {
+        res.status(500).send("gagal!");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("error");
+    });
+});
+
+app.get("/produk/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const db = dbService.getDbServiceInstance();
+
+  db.getProdukById(id)
+    .then((produk) => {
+      if (produk) {
+        res.render("produk/updateProduk", { produk });
+      } else {
+        res.status(404).send("error");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Errorr");
+    });
+});
+
+app.post("/produk/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { namaproduk, stock, hargasatuan } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.updateProdukById(id, namaproduk, stock, hargasatuan)
+    .then(res.redirect("/produk"))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error");
+    });
+});
+
+app.post("/produk/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.deleteProdukById(id);
+
+  result.then(res.redirect("/produk")).catch((err) => console.log(err));
+});
+
+//Transaksi
+app.get("/transaksi", (req, res) => {
+  const db = dbService.getDbServiceInstance();
+
+  db.getAllTransaksi()
+    .then((data) => {
+      if (!Array.isArray(data)) {
+        console.log("Error:", data);
+        data = [];
+      }
+      res.render("transaksi/indexTransaksi", { transaksi: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("transaksi/indexTransaksi", { transaksi: [] });
+    });
+});
+
+app.post("/transaksi/insert", (req, res) => {
+  const { idproduk, quantity, tanggal, hargatotal, id_karyawan } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.insertNewTransaksi(idproduk, quantity, tanggal, hargatotal, id_karyawan)
+    .then((insertedData) => {
+      if (insertedData) {
+        res.redirect("/transaksi");
+      } else {
+        res.status(500).send("gagal!");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("error");
+    });
+});
+
+// app.get("/transaksi/edit/:id", (req, res) => {
+//   const { id } = req.params;
+//   const db = dbService.getDbServiceInstance();
+
+//   db.getTransaksiById(id)
+//     .then((transaksi) => {
+//       if (transaksi) {
+//         res.render("updateTransaksi", { transaksi });
+//       } else {
+//         res.status(404).send("Erro");
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).send("Errorr");
+//     });
+// });
+
+app.get("/karyawan", (req, res) => {
+  const db = dbService.getDbServiceInstance();
+
+  db.getAllKaryawan()
+    .then((data) => {
+      if (!Array.isArray(data)) {
+        console.log("Error:", data);
+        data = [];
+      }
+      res.render("karyawan/indexKaryawan", { karyawans: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("karyawan/indexKaryawan", { karyawans: [] });
+    });
+});
+
+app.post("/karyawan/insert", (req, res) => {
+  const { nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.insertNewKaryawan(nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp)
+    .then((insertedData) => {
+      if (insertedData) {
+        res.redirect("/karyawan");
+      } else {
+        res.status(500).send("gagal!");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("error");
+    });
+});
+
+app.get("/karyawan/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const db = dbService.getDbServiceInstance();
+
+  db.getKaryawanById(id)
+    .then((karyawan) => {
+      if (karyawan) {
+        res.render("karyawan/updateKaryawan", { karyawan });
+      } else {
+        res.status(404).send("errror");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Errorr");
+    });
+});
+
+app.post("/karyawan/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.updateKaryawanById(id, nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp)
+    .then(res.redirect("/karyawan"))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error");
+    });
+});
+
+app.post("/karyawan/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.deleteKaryawanById(id);
+
+  result.then(res.redirect("/karyawan")).catch((err) => console.log(err));
+});
+
+app.get("/investor", (req, res) => {
+  const db = dbService.getDbServiceInstance();
+
+  db.getAllInvestor()
+    .then((data) => {
+      if (!Array.isArray(data)) {
+        console.log("Error:", data);
+        data = [];
+      }
+      res.render("investor/indexInvestor", { investors: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("investor/indexInvestor", { investors: [] });
+    });
+});
+
+app.post("/investor/insert", (req, res) => {
+  const { nama, jumlah } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.insertNewInvestor(nama, jumlah)
+    .then((insertedData) => {
+      if (insertedData) {
+        res.redirect("/investor");
+      } else {
+        res.status(500).send("gagal!");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("error");
+    });
+});
+
+app.get("/investor/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const db = dbService.getDbServiceInstance();
+
+  db.getInvestorById(id)
+    .then((investor) => {
+      if (investor) {
+        res.render("investor/updateinvestor", { investor });
+      } else {
+        res.status(404).send("errror");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Errorr");
+    });
+});
+
+app.post("/investor/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { nama, jumlah } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.updateInvestorById(id, nama, jumlah)
+    .then(res.redirect("/investor"))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error");
+    });
+});
+
+app.post("/investor/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.deleteInvestorById(id);
+
+  result.then(res.redirect("/investor")).catch((err) => console.log(err));
+});
+
+app.get("/user", (req, res) => {
+  const db = dbService.getDbServiceInstance();
+
+  db.getAllUser()
+    .then((data) => {
+      if (!Array.isArray(data)) {
+        console.log("Error:", data);
+        data = [];
+      }
+      res.render("user/indexUser", { users: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("user/indexUser", { users: [] });
+    });
+});
+
+app.post("/user/insert", (req, res) => {
+  const { nama, username, password, kontak, role } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.insertNewUser(nama, username, password, kontak, role)
+    .then((insertedData) => {
+      if (insertedData) {
+        res.redirect("/user");
+      } else {
+        res.status(500).send("gagal!");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("error");
+    });
+});
+
+app.get("/user/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const db = dbService.getDbServiceInstance();
+
+  db.getUserById(id)
+    .then((user) => {
+      if (user) {
+        res.render("user/updateuser", { user });
+      } else {
+        res.status(404).send("errror");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Errorr");
+    });
+});
+
+app.post("/user/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { nama, username, password, kontak, role } = req.body;
+  const db = dbService.getDbServiceInstance();
+
+  db.updateUserById(id, nama, username, password, kontak, role)
+    .then(res.redirect("/user"))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error");
+    });
+});
+
+app.post("/user/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.deleteUserById(id);
+
+  result.then(res.redirect("/user")).catch((err) => console.log(err));
+});
+
+app.listen(process.env.PORT, () => console.log("APP IS RUNNING"));
---
+++ server/dbService.js
@@ -0,0 +1,469 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+let instance = null;
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err.message);
+  }
+  console.log("db " + connection.state);
+});
+
+class DbService {
+  static getDbServiceInstance() {
+    return instance ? instance : new DbService();
+  }
+
+  // PRODUK Methods
+  async getAllProduk() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM dataproduk;";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async getAllTransaksi() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM datatransaksi;";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async getAllKaryawan() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM data_karyawan;";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async getAllInvestor() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM data_investor;";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async getAllUser() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM data_user;";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async insertNewProduk(namaproduk, stock, hargasatuan) {
+    try {
+      const insertId = await new Promise((resolve, reject) => {
+        const query = "INSERT INTO dataproduk (namaproduk, stock, hargasatuan) VALUES (?,?,?);";
+        connection.query(query, [namaproduk, stock, hargasatuan], (err, result) => {
+          if (err) {
+            reject(new Error(err.message));
+          } else {
+            resolve(result.insertId);
+          }
+        });
+      });
+      return {
+        id_produk: insertId,
+        namaproduk: namaproduk,
+        stock: stock,
+        hargasatuan: hargasatuan,
+      };
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async insertNewTransaksi(id_produk, quantity, tanggal, id_karyawan) {
+    try {
+      const hargaSatuan = await new Promise((resolve, reject) => {
+        const queryHarga = "SELECT hargasatuan FROM dataproduk WHERE id_produk = ?;";
+        connection.query(queryHarga, [id_produk], (err, result) => {
+          if (err) {
+            console.error("Error fetching harga satuan:", err.message);
+            reject(new Error(err.message));
+          } else {
+            resolve(result[0].hargasatuan);
+          }
+        });
+      });
+
+      const hargatotal = hargaSatuan * quantity;
+      const insertId = await new Promise((resolve, reject) => {
+        const query = "INSERT INTO datatransaksi (id_produk, quantity, tanggal, hargatotal, id_karyawan) VALUES (?,?,?,?,?);";
+        connection.query(query, [id_produk, quantity, tanggal, hargatotal, id_karyawan], (err, result) => {
+          if (err) {
+            console.error("Error inserting new transaksi:", err.message);
+            reject(new Error(err.message));
+          } else {
+            resolve(result.insertId);
+          }
+        });
+      });
+
+      return {
+        id: insertId,
+        id_produk: id_produk,
+        quantity: quantity,
+        tanggal: tanggal,
+        hargatotal: hargatotal,
+        id_karyawan: id_karyawan,
+      };
+    } catch (error) {
+      console.error("Transaction insertion failed:", error);
+      return null;
+    }
+  }
+
+  async insertNewKaryawan(nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp) {
+    try {
+      const insertId = await new Promise((resolve, reject) => {
+        const query = "INSERT INTO data_karyawan (nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp) VALUES (?,?,?,?,?);";
+        connection.query(query, [nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp], (err, result) => {
+          if (err) {
+            reject(new Error(err.message));
+          } else {
+            resolve(result.insertId);
+          }
+        });
+      });
+      return {
+        id: insertId,
+        nama_karyawan: nama_karyawan,
+        tgl_lahir: tgl_lahir,
+        jenis_kelamin: jenis_kelamin,
+        alamat: alamat,
+        noTlp: noTlp,
+      };
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async insertNewInvestor(nama, jumlah) {
+    try {
+      const insertId = await new Promise((resolve, reject) => {
+        const query = "INSERT INTO data_investor (nama, jumlah) VALUES (?,?);";
+        connection.query(query, [nama, jumlah], (err, result) => {
+          if (err) {
+            reject(new Error(err.message));
+          } else {
+            resolve(result.insertId);
+          }
+        });
+      });
+      return {
+        id: insertId,
+        nama: nama,
+        jumlah: jumlah,
+      };
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async insertNewUser(nama, username, password, kontak, role) {
+    try {
+      const insertId = await new Promise((resolve, reject) => {
+        const query = "INSERT INTO data_user (nama, username, password, kontak, role) VALUES (?,?,?,?,?);";
+        connection.query(query, [nama, username, password, kontak, role], (err, result) => {
+          if (err) {
+            reject(new Error(err.message));
+          } else {
+            resolve(result.insertId);
+          }
+        });
+      });
+      return {
+        id: insertId,
+        nama: nama,
+        username: username,
+        password: password,
+        kontak: kontak,
+        role: role,
+      };
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async deleteKaryawanById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "DELETE FROM data_karyawan WHERE id = ?;";
+
+        connection.query(query, [id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return response === 1;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async deleteProdukById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "DELETE FROM dataproduk WHERE id_produk = ?;";
+
+        connection.query(query, [id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return response === 1;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async deleteInvestorById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "DELETE FROM data_investor WHERE id = ?;";
+
+        connection.query(query, [id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return response === 1;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async deleteUserById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "DELETE FROM data_user WHERE id = ?;";
+
+        connection.query(query, [id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return response === 1;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async getProdukById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM dataproduk WHERE id_produk = ?";
+
+        connection.query(query, [id], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results[0]);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async getKaryawanById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM data_karyawan WHERE id = ?";
+
+        connection.query(query, [id], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results[0]);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async getInvestorById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM data_investor WHERE id = ?";
+
+        connection.query(query, [id], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results[0]);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async getUserById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM data_user WHERE id = ?";
+
+        connection.query(query, [id], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results[0]);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async updateProdukById(id, namaproduk, stock, hargasatuan) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "UPDATE dataproduk SET namaproduk = ?, stock = ?,hargasatuan = ? WHERE id_produk = ?;";
+
+        connection.query(query, [namaproduk, stock, hargasatuan, id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return response === 1;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async updateKaryawanById(id, nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "UPDATE data_karyawan SET nama_karyawan = ?, tgl_lahir = ?,jenis_kelamin = ?, alamat = ?, noTlp= ? WHERE id = ?;";
+
+        connection.query(query, [nama_karyawan, tgl_lahir, jenis_kelamin, alamat, noTlp, id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return response === 1;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async updateInvestorById(id, nama, jumlah) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "UPDATE data_investor SET nama = ?, jumlah = ? WHERE id = ?;";
+
+        connection.query(query, [nama, jumlah, id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return response === 1;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async updateUserById(id, nama, username, password, kontak, role) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "UPDATE data_user SET nama = ?, username = ?, password = ?, kontak = ?, role = ? WHERE id = ?;";
+
+        connection.query(query, [nama, username, password, kontak, role, id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return response === 1;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
+
+module.exports = DbService;
